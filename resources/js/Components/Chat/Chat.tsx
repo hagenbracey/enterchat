61215f45ad0e2447e4f44b36d577aa3f9cf3c9eb
Hagen Bracey <hagen.bracey@gmail.com>
2025-02-14 10:46:17 -0600

    updated readme and routing, and added Chat component

---
+++ resources/js/Components/Chat/Chat.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Chat() {
+    return (
+        <div className="p-6 text-white mx-auto my-auto">
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
+
+export default Chat;
