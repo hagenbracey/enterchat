ed59852a6d162eeef64f0179abcbf00dca002bde
Hagen Bracey <hagen.bracey@gmail.com>
2025-02-17 12:16:52 -0600

    Basic layout for chat window implemented and got it running on my Arch machine for development... here goes lol

--- resources/js/Components/Chat/Chat.tsx
+++ resources/js/Components/Chat/Chat.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Chat() {
     return (
-        <div className="p-6 text-white mx-auto my-auto">
-            <h1>Hello, world!</h1>
-        </div>
+        <>
+            <div className="p-6 text-white mx-auto my-auto min-h-96">
+                <p>Welcome, Hagen!</p>
+            </div>
+        </>
     );
 }
 
